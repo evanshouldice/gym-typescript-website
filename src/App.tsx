a25dc9fcb5a195dcfd30a0e23f58db74bcc293c9
Evan Shouldice <e.shouldice@gmail.com>
2023-08-09 21:12:34 -0400

    Added Benefits Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
 import { SelectedPage } from "@/shared/types";
+
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
@@ -29,6 +31,7 @@ function App() {
       setSelectedPage={setSelectedPage}
     />
     <Home setSelectedPage={setSelectedPage}/>
+    <Benefits setSelectedPage={setSelectedPage}/>
   </div>;
 }
 
