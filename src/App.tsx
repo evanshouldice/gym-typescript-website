f17651a419a440a0a3a3a5c5c260d913ca574f66
Evan Shouldice <e.shouldice@gmail.com>
2023-08-11 16:34:14 -0400

    Added Contact Us Page with form

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses";
 import { SelectedPage } from "@/shared/types";
+import ContactUs from "./scenes/contactUs";
 
 
 function App() {
@@ -34,6 +35,7 @@ function App() {
     <Home setSelectedPage={setSelectedPage}/>
     <Benefits setSelectedPage={setSelectedPage}/>
     <OurClasses setSelectedPage={setSelectedPage}/>
+    <ContactUs setSelectedPage={setSelectedPage}/>
   </div>;
 }
 
